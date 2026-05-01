324c458d353cdc4ce69641b66751c0c7bf9ca65d
SxhkdTafe <30105350@tafe.wa.edu.au>
2026-05-01 10:27:19 +0800

    Finished backend skeleton and frontend general Got implementation of generation left I estimate maybe a day or two of work left

--- app/src/main/java/com/example/aijournalcompanion/MainActivity.kt
+++ app/src/main/java/com/example/aijournalcompanion/MainActivity.kt
@@ -8,7 +8,10 @@ import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +21,9 @@ import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import java.net.HttpURLConnection
+import java.net.URI
+import com.google.gson.Gson
 
 
 class MainActivity : AppCompatActivity(),  OnItemSelectedListener {
@@ -27,14 +33,22 @@ class MainActivity : AppCompatActivity(),  OnItemSelectedListener {
     private var sortChoices = arrayOf(
         "Bubble Sort", "Insertion Sort", "Selection Sort"
     )
-
+    private val listItems = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val apiUrl = "http://127.0.0.1:8000/emotion_parse"
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val showButton = findViewById<Button>(R.id.Show_button)
         val searchSpin = findViewById<Spinner>(R.id.Search_spinner)
         val sortSpin = findViewById<Spinner>(R.id.Sort_spinner)
+        val userInput = findViewById<EditText>(R.id.editText)
+        val analyseBtn = findViewById<Button>(R.id.Analyse_button)
+        val journalListView = findViewById<ListView>(R.id.journalListView)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
         val adSearch: ArrayAdapter<*> = ArrayAdapter<Any?>(this,
             android.R.layout.simple_spinner_item, searchChoices
         )
@@ -45,6 +59,15 @@ class MainActivity : AppCompatActivity(),  OnItemSelectedListener {
         adSort.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         searchSpin.adapter = adSearch
         sortSpin.adapter = adSort
+
+
+        analyseBtn.setOnClickListener {
+            val result = displayInput(userInput, apiUrl)
+            listItems.add(result)
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,listItems)
+            journalListView.adapter= adapter
+            resultTextView.text = result
+        }
         showButton.setOnClickListener{
             showPopup()
         }
@@ -82,7 +105,29 @@ class MainActivity : AppCompatActivity(),  OnItemSelectedListener {
         btnClose.setOnClickListener {
             dialog.dismiss()
         }
-
         dialog.show()
+    }
+    private fun displayInput(box: EditText, url: String) : String{
+        if(box.text == null || box.text.isEmpty()){
+            return "Textbox is empty"
+        }
+        val input = box.text.trim().toString()
+        try {
+            val gson = Gson()
+            val uri = URI(url)
+            val conn = uri.toURL().openConnection() as HttpURLConnection
+            conn.requestMethod = "POST"
+            conn.setRequestProperty("Content-Type", "application/json")
+            conn.doInput = true
+            val json = gson.toJson(mapOf("text" to input))
+            conn.getOutputStream().use {it.write((json.toByteArray()))}
+
+            val response = conn.getInputStream().bufferedReader().readText()
+            val obj = gson.fromJson(response, Map::class.java)
+            return obj["Advice"]?.toString() ?: "(No response)"
+        }
+        catch (e : Exception){
+            return "Error: couldn't connect to server | $e "
+        }
     }
 }
